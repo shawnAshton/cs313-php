1a32c1875298e17f434b1eaaec41d6f8b366dada
shawnAshton <ash14009@byui.edu>
2018-09-27 16:39:24 -0600

    hero

--- web/navTA.php
+++ web/navTA.php
@@ -10,7 +10,7 @@
 
 
 
-         <a href="homeTA.php">HOME</a>
+         <a class="currentPage" href="homeTA.php">HOME</a> 
          <a href="about-usTA.php">ABOUT US</a>
          <a href="loginTA.php">LOGIN</a>
       </nav>
