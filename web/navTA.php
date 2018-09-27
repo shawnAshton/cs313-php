e2bdd092c80ce040fc919886260565869399c35c
shawnAshton <ash14009@byui.edu>
2018-09-27 16:20:10 -0600

    asd

--- web/navTA.php
+++ web/navTA.php
@@ -2,7 +2,17 @@
 
       <nav>
          <h1>BATMAN BRAWLERS INC.</h1><br/>
-         <a href="homeTA.php">HOME</a>
+         <?php 
+            $home = "homeTA.php"
+            echo "<a href='$home'>HOME</a>";
+
+
+         ?>
+
+
+
+
+         <!-- <a href="homeTA.php">HOME</a>
          <a href="about-usTA.php">ABOUT US</a>
-         <a href="loginTA.php">LOGIN</a>
+         <a href="loginTA.php">LOGIN</a> -->
       </nav>
