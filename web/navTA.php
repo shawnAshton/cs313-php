47a309ff4a06a2a585da57d9ef4d2ef18afa65e3
shawnAshton <ash14009@byui.edu>
2018-09-27 16:35:51 -0600

    asd

--- web/navTA.php
+++ web/navTA.php
@@ -3,16 +3,14 @@
       <nav>
          <h1>BATMAN BRAWLERS INC.</h1><br/>
          <?php 
-   
-            echo "<a href='homeTA" . ".php'>HOME</a>";
-
-
+            $file = basename($_SERVER["SCRIPT_FILENAME"], '.php');
+            echo "$file";
          ?>
 
 
 
 
-         <!-- <a href="homeTA.php">HOME</a>
+         <a href="homeTA.php">HOME</a>
          <a href="about-usTA.php">ABOUT US</a>
-         <a href="loginTA.php">LOGIN</a> -->
+         <a href="loginTA.php">LOGIN</a>
       </nav>
