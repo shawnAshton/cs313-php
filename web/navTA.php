72aa051b9b890394eaf85d5f3f2496d3810a2255
shawnAshton <ash14009@byui.edu>
2018-09-27 16:48:59 -0600

    aa

--- web/navTA.php
+++ web/navTA.php
@@ -10,7 +10,18 @@
 
 
 
-         <a class="currentPage" href="homeTA.php">HOME</a> 
+         <a
+         <?php 
+         if($file === 'homeTA') 
+            echo "class='currentPage'";
+         ?>
+
+         href="homeTA.php">HOME</a> 
+
+
+
+
+
          <a href="about-usTA.php">ABOUT US</a>
          <a href="loginTA.php">LOGIN</a>
       </nav>
