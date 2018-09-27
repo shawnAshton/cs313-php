ae37e5ed14c89fac2d87d543d3cd186d054ee252
shawnAshton <ash14009@byui.edu>
2018-09-27 14:15:42 -0600

    WEB

---
+++ web/home.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<htnl lang = "en">
+   <head>
+      <meta charset="utf-8">
+      <link rel="stylesheet" type="text/css" href="home.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Shawn's Home Page</title>
+      <script></script>
+   </head>
+
+
+   <body>
+      <ul>
+         <li><a href="hello.html">To Be continued</a></li>
+         <li><a href="hello.html">To Be continued</a></li>
+         <li><a href="hello.html">To Be continued</a></li>
+         <li><a href="hello.html">To Be continued</a></li>
+         <li><a href="hello.html">To Be continued</a></li>
+         <li><a href="hello.html">To Be continued</a></li>
+         <li><a href="hello.html">To Be continued</a></li>
+         <li><a href="hello.html">To Be continued</a></li>
+         <li><a href="hello.html">To Be continued</a></li>
+         <li><a href="hello.html">To Be continued</a></li>
+         <li><a href="hello.html">To Be continued</a></li>
+   </body>
