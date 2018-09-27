06fcbbcae756da5ce57672843c14b701bf2408e4
shawnAshton <ash14009@byui.edu>
2018-09-27 16:05:15 -0600

    asd

--- web/navTA.php
+++ web/navTA.php
@@ -1,18 +1,7 @@
-<!doctype html>
-<htnl lang = "en">
-   <head>
-      <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="TA.css">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title></title>
-      <script></script>
-   </head>
 
 
-   <body>
       <nav>
          <a href="homeTA.php">HOME</a>
          <a href="about-usTA.php">ABOUT US</a>
          <a href="loginTA.php">LOGIN</a>
       </nav>
-   </body>
