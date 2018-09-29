db28a6adf454d21568798384bb731ab88c85232d
shawnAshton <ash14009@byui.edu>
2018-09-29 15:45:12 -0600

    a

--- web/assignments.php
+++ web/assignments.php
@@ -4,13 +4,13 @@
       <meta charset="utf-8">
       <link rel="stylesheet" type="text/css" href="myHome.css">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Shawn's Home Page!</title>
+      <title>Shawn's assignment Page!</title>
       <script></script>
    </head>
 
 
    <body>
-      <header>THIS IS MY HOME PAGE!</header>
+      <header>THESE ARE MY ASSIGNMENTS!</header>
       <?php
          include 'nav.php';
       ?>
