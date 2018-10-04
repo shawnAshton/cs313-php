275f871602135fab2bbd6eb4b9932c99c64794a1
shawnAshton <ash14009@byui.edu>
2018-10-04 15:59:13 -0600

    a

---
+++ web/sessions.php
@@ -0,0 +1,14 @@
+<?php
+setcookie("fav-text", "c is for cookie", time() + (86400 * 7));
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title></title>
+</head>
+<body>
+<p>this is a body</p>
+</body>
+</html>
