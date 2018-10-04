4c6eb3e31a2a4dd66050202ee1af5d47950e1095
shawnAshton <ash14009@byui.edu>
2018-10-04 16:40:10 -0600

    a

--- web/sessions.php
+++ web/sessions.php
@@ -23,6 +23,6 @@ if (isset($_SESSION["count"])){
    <title></title>
 </head>
 <body>
-<p>this is how many times you visited: <?php echo $_SESSION["count"]?></p>
+<p>this is how many times you visited: <?php echo $SESSION["count"]?></p>
 </body>
 </html>
