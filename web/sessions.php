f545d19b5fd3553f8914076369533b252479d2e5
shawnAshton <ash14009@byui.edu>
2018-10-04 16:19:09 -0600

    a

--- web/sessions.php
+++ web/sessions.php
@@ -1,5 +1,13 @@
 <?php
 setcookie("fav-text", "c is for cookie", time() + (86400 * 7));
+session_start();
+$count = 0;
+if (isset($_SESSION["count"])){
+   $_SESSION["count"] = $_SESSION["count"]++; 
+}else{
+   $_SESSION["count"] = $count;
+}
+
 ?>
 
 
@@ -9,6 +17,6 @@ setcookie("fav-text", "c is for cookie", time() + (86400 * 7));
    <title></title>
 </head>
 <body>
-<p>this is a body</p>
+<p>this is how many times you visited: <?php echo $SESSION["count"]?></p>
 </body>
 </html>
