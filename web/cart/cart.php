a4b2fc9813d4a289d98364a34d1732ad079580fa
shawnAshton <ash14009@byui.edu>
2018-10-07 17:42:46 -0600

    a

--- web/cart/cart.php
+++ web/cart/cart.php
@@ -1,11 +1,27 @@
-   
 
-   <!DOCTYPE html>
-   <html>
-   <head>
-      <title></title>
-   </head>
-   <body>
-   
-   </body>
-   </html>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+
+<?php
+// setcookie("fav-text", "c is for cookie", time() + (86400 * 7));
+session_start();
+$count = 1;
+if (isset($_SESSION["count"])){
+   $_SESSION["count"]++; 
+}else{
+   $_SESSION["count"] = $count;
+}
+
+?>
+
+
+
+   <title></title>
+</head>
+<body>
+<p>this is how many times you visited: <?php echo $_SESSION["count"]?></p>
+</body>
+</html>
