bda8e591e0a6ad8f5cf81e3fe80f84fbb92f0f5b
shawnAshton <ash14009@byui.edu>
2018-10-07 17:58:36 -0600

    a

--- web/cart/cart.php
+++ web/cart/cart.php
@@ -22,6 +22,17 @@ if (isset($_SESSION["count"])){
    <title></title>
 </head>
 <body>
+   <form>
+      <input type="checkbox" name="purchase[]" value="apple">Apple<br>
+      <input type="checkbox" name="purchase[]" value="Banana">Banana<br>
+      <input type="checkbox" name="purchase[]" value="Curry">Curry<br>
+      <input type="checkbox" name="purchase[]" value="Chicke">Chicken<br>
+      <input type="checkbox" name="purchase[]" value="French frie">French frie<br>
+      <input type="checkbox" name="purchase[]" value="Small hot dog">Small hot dog<br>
+      <input type="checkbox" name="purchase[]" value="Rasberry Pie">Rasberry Pie<br>
+      <input type="checkbox" name="purchase[]" value="Dog food">Dog food<br>
+      <input type="checkbox" name="purchase[]" value="lollipo">lollipop<br>
+   </form>
 <p>this is how many times you visited: <?php echo $_SESSION["count"]?></p>
 </body>
 </html>
