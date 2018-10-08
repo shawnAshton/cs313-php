0b61243c4da69098d8a3b1c214c6b21626571b4c
shawnAshton <ash14009@byui.edu>
2018-10-07 20:17:25 -0600

    a

--- web/cart/update.php
+++ web/cart/update.php
@@ -1,30 +1,8 @@
 
-        <?php 
-        $count = sizeof($_POST["purchase"]);
-        $oneOrLess = false;
-        if ($count <= 1){
-            $oneOrLess = true;
-        }else{
-            $oneOrLess = false;
-        }
-        if($oneOrLess == true && $count == 1){
-            echo "You have picked: $count item<br>";
-        }else{
-            echo "You have picked: $count items<br>";
-        }
-        
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                foreach($_POST["purchase"] as $purchase){
-                     
-                     if(($count == 1) && ($oneOrLess == false)){
-                        echo "<br>and,";
-                        echo "<br>a $purchase";
-                     }else{
-                        echo "<br>$purchase";
-                     }
-                     
-                     $count--;
-                }
-            }
-        ?>
- 
+<?php 
+   echo "got into php"
+   foreach($_POST["purchase"] as $purchase){                 
+      echo "<br>$purchase";
+
+?>
+
