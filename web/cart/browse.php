ef9de40764151a3e7002843e559514460b839645
shawnAshton <ash14009@byui.edu>
2018-10-07 18:45:10 -0600

    a

--- web/cart/browse.php
+++ web/cart/browse.php
@@ -16,7 +16,7 @@
       <input type="checkbox" name="purchase[]" value="Banana">Banana<br>
       <input type="checkbox" name="purchase[]" value="Curry">Curry<br>
       <input type="checkbox" name="purchase[]" value="Chicken">Chicken<br>
-      <input type="checkbox" name="purchase[]" value="French frie">French frie<br>
+      <input type="checkbox" name="purchase[]" value="French fry">French fry<br>
       <input type="checkbox" name="purchase[]" value="Small hot dog">Small hot dog<br>
       <input type="checkbox" name="purchase[]" value="Rasberry Pie">Rasberry Pie<br>
       <input type="checkbox" name="purchase[]" value="Dog food">Dog food<br>
