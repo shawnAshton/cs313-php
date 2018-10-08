09817b9a5c67349f4007bbfac8a1106e16d2c766
shawnAshton <ash14009@byui.edu>
2018-10-07 20:23:16 -0600

    a

--- web/cart/update.php
+++ web/cart/update.php
@@ -1,8 +1,17 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+   <title></title>
+</head>
+<body>
+
 <?php 
    echo "got into php"
    foreach($_POST["purchase"] as $purchase){                 
       echo "<br>$purchase";
 
 ?>
+</body>
+</html>
 
