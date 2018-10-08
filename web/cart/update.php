579c4f3213d7427a885f44ff894a9d1f4bb0e170
shawnAshton <ash14009@byui.edu>
2018-10-07 20:25:47 -0600

    a

--- web/cart/update.php
+++ web/cart/update.php
@@ -1,17 +1,46 @@
 
-<!DOCTYPE html>
+
 <html>
 <head>
-   <title></title>
+    <title>Your Cart</title>
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
 
-<?php 
-   echo "got into php";
-   foreach($_POST["purchase"] as $purchase){                 
-      echo "<br>$purchase";
+    <h1>Your Cart Results </h1>
 
-?>
+    <section>
+    
+        <?php 
+        $count = sizeof($_POST["purchase"]);
+        $oneOrLess = false;
+        if ($count <= 1){
+            $oneOrLess = true;
+        }else{
+            $oneOrLess = false;
+        }
+        if($oneOrLess == true && $count == 1){
+            echo "You have picked: $count item<br>";
+        }else{
+            echo "You have picked: $count items<br>";
+        }
+        
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                foreach($_POST["purchase"] as $purchase){
+                     
+                     if(($count == 1) && ($oneOrLess == false)){
+                        echo "<br>and,";
+                        echo "<br>a $purchase";
+                     }else{
+                        echo "<br>$purchase";
+                     }
+                     
+                     $count--;
+                }
+            }
+        ?>
+ 
+     </section>
+    
 </body>
 </html>
-
