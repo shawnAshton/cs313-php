92b7eefced02128576f8cbf1075e35fff442953b
shawnAshton <ash14009@byui.edu>
2018-10-07 18:06:47 -0600

    a

---
+++ web/cart/browse.php
@@ -0,0 +1,38 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+
+<?php
+// setcookie("fav-text", "c is for cookie", time() + (86400 * 7));
+session_start();
+$count = 1;
+if (isset($_SESSION["count"])){
+   $_SESSION["count"]++; 
+}else{
+   $_SESSION["count"] = $count;
+}
+
+?>
+
+
+
+   <title></title>
+</head>
+<body>
+   <form action="cart.php" method="post">
+      <input type="checkbox" name="purchase[]" value="apple">Apple<br>
+      <input type="checkbox" name="purchase[]" value="Banana">Banana<br>
+      <input type="checkbox" name="purchase[]" value="Curry">Curry<br>
+      <input type="checkbox" name="purchase[]" value="Chicke">Chicken<br>
+      <input type="checkbox" name="purchase[]" value="French frie">French frie<br>
+      <input type="checkbox" name="purchase[]" value="Small hot dog">Small hot dog<br>
+      <input type="checkbox" name="purchase[]" value="Rasberry Pie">Rasberry Pie<br>
+      <input type="checkbox" name="purchase[]" value="Dog food">Dog food<br>
+      <input type="checkbox" name="purchase[]" value="lollipo">lollipop<br>
+   </form>
+<p>this is how many times you visited: <?php echo $_SESSION["count"]?></p>
+</body>
+</html>
--- web/cart/cart.php
+++ web/cart/cart.php
@@ -1,38 +1,25 @@
 
 
-<!DOCTYPE html>
 <html>
 <head>
-
-
-<?php
-// setcookie("fav-text", "c is for cookie", time() + (86400 * 7));
-session_start();
-$count = 1;
-if (isset($_SESSION["count"])){
-   $_SESSION["count"]++; 
-}else{
-   $_SESSION["count"] = $count;
-}
-
-?>
-
-
-
-   <title></title>
+    <title>cart</title>
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-   <form>
-      <input type="checkbox" name="purchase[]" value="apple">Apple<br>
-      <input type="checkbox" name="purchase[]" value="Banana">Banana<br>
-      <input type="checkbox" name="purchase[]" value="Curry">Curry<br>
-      <input type="checkbox" name="purchase[]" value="Chicke">Chicken<br>
-      <input type="checkbox" name="purchase[]" value="French frie">French frie<br>
-      <input type="checkbox" name="purchase[]" value="Small hot dog">Small hot dog<br>
-      <input type="checkbox" name="purchase[]" value="Rasberry Pie">Rasberry Pie<br>
-      <input type="checkbox" name="purchase[]" value="Dog food">Dog food<br>
-      <input type="checkbox" name="purchase[]" value="lollipo">lollipop<br>
-   </form>
-<p>this is how many times you visited: <?php echo $_SESSION["count"]?></p>
+
+    <h1 class="glow"> Result </h1>
+
+    <section id="main">
+    
+        <?php 
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                foreach($_POST["purchase"] as $purchase){
+                    echo "<br>You have picked $purchase";
+                }
+            }
+        ?>
+ 
+     </section>
+    
 </body>
 </html>
