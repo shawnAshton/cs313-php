e490b434323c5743e38e496dd858e9eb5fb5f953
shawnAshton <ash14009@byui.edu>
2018-10-08 20:36:56 -0600

    a

--- web/cart/checkout.php
+++ web/cart/checkout.php
@@ -8,38 +8,13 @@
 
     <h1>Your Cart Results</h1>
 
-    <section>
-    
-        <?php 
-        session_start();
-        $count = sizeof($_SESSION["purchase"]);
-        $oneOrLess = false;
-        if ($count <= 1){
-            $oneOrLess = true;
-        }else{
-            $oneOrLess = false;
-        }
-        if($oneOrLess == true && $count == 1){
-            echo "You have picked: $count item<br>";
-        }else{
-            echo "You have picked: $count items<br>";
-        }
-    
-            foreach($_SESSION["purchase"] as $purchase){
-                 
-                 if(($count == 1) && ($oneOrLess == false)){
-                    echo "<br>and,";
-                    echo "<br>a $purchase<br><br>";
-                 }else{
-                    echo "<br>$purchase";
-                 }
-                
-                 $count--;
-            }
-            
-        ?>
+    <form>
+        <textarea name="address">
+
+        </textarea>
+
  
-     </section>
+     </form>
 
 
     
