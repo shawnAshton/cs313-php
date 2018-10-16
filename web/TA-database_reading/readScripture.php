4c0cd5522f9720f6d17a587e2db3882f82f7cbe0
shawnAshton <ash14009@byui.edu>
2018-10-16 15:56:08 -0600

    a

---
+++ web/TA-database_reading/readScripture.php
@@ -0,0 +1,38 @@
+<?php
+   try
+   {
+     $dbUrl = getenv('DATABASE_URL');
+
+     $dbOpts = parse_url($dbUrl);
+
+     $dbHost = $dbOpts["host"];
+     $dbPort = $dbOpts["port"];
+     $dbUser = $dbOpts["user"];
+     $dbPassword = $dbOpts["pass"];
+     $dbName = ltrim($dbOpts["path"],'/');
+
+     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   }
+   catch (PDOException $ex)
+   {
+     echo 'Error!: ' . $ex->getMessage();
+     die();
+   }
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title></title>
+</head>
+<body>
+   <?php
+      foreach ($db->query('select * FROM scripture') as $script) { //this is bad practice...only display what u need
+         echo "<b> " . $script['book'] . "</b> " . $script['chapter'] . ": " . $script['verse']. "<br>" . $script['content'] . "<br>";
+      }
+   ?>
+</body>
+</html>
