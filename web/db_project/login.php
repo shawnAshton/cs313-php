b95db83fa2f9794a40aade849869541a2b7f5dbc
shawnAshton <ash14009@byui.edu>
2018-10-20 20:14:57 -0600

    a

--- web/db_project/login.php
+++ web/db_project/login.php
@@ -7,6 +7,7 @@
    <form action="displayDB.php" method="POST">
       Username: <input type="text" name="username"><br>
       Password: <input type="text" name="password"><br>
+      <input type="submit" name="submit">
    </form>
 </body>
 </html>
