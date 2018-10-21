5eccc6514d41b5c1c38c1bce6e7dfd564c3c6a1d
shawnAshton <ash14009@byui.edu>
2018-10-20 19:44:59 -0600

    a

--- web/db_project/displayDB.php
+++ web/db_project/displayDB.php
@@ -29,8 +29,8 @@ $projects = $stmt->fetchAll(PDO::FETCH_ASSOC);
    <ul>
 <?php
 foreach ($projects as $project) {
-   $name = $project['name'];
-   $job = $movie['job_title'];
+   $name = $project['w.name'];
+   $job = $movie['j.job_title'];
    echo "<li><p>$name ($job)</p></li>";
 }
 ?>
