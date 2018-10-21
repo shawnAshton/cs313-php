859577be1528d50013f9c10368b3e42acb4f87a3
shawnAshton <ash14009@byui.edu>
2018-10-20 22:27:00 -0600

    a

--- web/db_project/displayDB.php
+++ web/db_project/displayDB.php
@@ -9,16 +9,26 @@ $stmt->execute();
 $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 // lets see if the user has a project
 $currentUser = FALSE;
+$passwordError = FALSE;
 foreach ($users as $user) {
    if ($user['username'] == $usernamePassed)
    {
       $currentUser = TRUE;
+      if($user['password'] != $passwordPassed)
+      {
+         $passwordError = TRUE;
+      }
    }
 }
 if ($currentUser == FALSE)
 {
-   echo "you are not a user in the system";
-   header("location: login.php");
+   echo "$usernamePassed is not a recognized username.";
+ //  header("location: login.php");
+}
+if ($passwordError == TRUE && $currentUser == TRUE)
+{
+   echo "This password does not match the username's password.";
+ //  header("location: login.php");
 }
 
 if ($currentUser)
