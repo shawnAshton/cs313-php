7c1032bea997502d51fd4abe216dd58b931118fe
shawnAshton <ash14009@byui.edu>
2018-10-24 19:39:52 -0600

    a

--- web/dynamicInputNotes.php
+++ web/dynamicInputNotes.php
@@ -16,7 +16,7 @@
 
          divTag.appendChild(newTextBox);
          divTag.insertBefore(document.createElement("BR"), button);
-         divTag.appendChild(document.createTextNode("X"))
+         divTag.insertBefore(document.createTextNode("X"), button);
          divTag.insertBefore(newTextBox, button);
       }
 
