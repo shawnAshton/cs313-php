a4949c09ef32086adf73fc0d9022faa8458c86b0
shawnAshton <ash14009@byui.edu>
2018-10-25 21:20:43 -0600

    a

--- web/db_project/insertProject.php
+++ web/db_project/insertProject.php
@@ -1,18 +1,29 @@
 <?php
 // connect to db
 session_start();
+//get variables...
 $user = $_SESSION["user"];
 $user_id = $_SESSION["user_id"];
-$projectTitle = $_GET["projectTitle"];
-$totalMeetings = $_GET["totalMeetings"];
-$names = $_GET["names"];
-$jobs = $_GET["jobs"];
+$projectTitle = $_POST["projectTitle"];
+$totalMeetings = $_POST["totalMeetings"];
+$names = $_POST["names"];
+$jobs = $_POST["jobs"];
+
+//cleanse variables
+$user = htmlspecialchars($user);
+$user_id = htmlspecialchars($user_id);
+$projectTitle = htmlspecialchars($projectTitle);
+$totalMeetings = htmlspecialchars($totalMeetings);
+//the names and jobs array will be cleansed a little later...
+
 require('dbConnect.php');
 $db = get_db();
-
-echo "USER ID: $user_id;<br>";
 //insert project into database
-
+$query = "INSERT INTO project(program_user_id, title) VALUES (:user_id, :projectTitle);";
+$stmt = $db->prepare($query);
+$stmt->bindValue(":user_id", $user_id, PDO::PARAM_INT);
+$stmt->bindValue(":projectTitle", $projectTitle, PDO::PARAM_STR);
+$stmt->execute();
 //insert names into worker
 
 //insert jobs into job
