007a585de73f0e8d5f8c8b2ff1e34005ed13442f
shawnAshton <ash14009@byui.edu>
2018-10-25 19:28:56 -0600

    a

--- web/db_project/displayDB.php
+++ web/db_project/displayDB.php
@@ -1,42 +1,53 @@
 <?php
+session_start();
+
 // connect to db
 require('dbConnect.php');
 $db = get_db();
-$usernamePassed = $_POST['username'];
-$passwordPassed = $_POST['password'];
-$usernamePassed = htmlspecialchars($usernamePassed);
-$stmt = $db->prepare("SELECT username, password FROM program_user;");
-// I NEED TO BIND THINGS THAT I USE...
-$stmt->execute();
-$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// lets see if the user has a project
-$currentUser = FALSE;
-$passwordError = FALSE;
-foreach ($users as $user) {
-   if ($user['username'] == $usernamePassed)
-   {
-      $currentUser = TRUE;
-      if($user['password'] != $passwordPassed)
+if(!(isset($_SESSION["user"])))
+{
+   $usernamePassed = $_POST['username'];
+   $passwordPassed = $_POST['password'];
+
+   $usernamePassed = htmlspecialchars($usernamePassed);
+   $stmt = $db->prepare("SELECT username, password FROM program_user;");
+   // I NEED TO BIND THINGS THAT I USE...
+   $stmt->execute();
+   $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   // lets see if the user has a project
+   $currentUser = FALSE;
+   $passwordError = FALSE;
+   foreach ($users as $user) {
+      if ($user['username'] == $usernamePassed)
       {
-         $passwordError = TRUE;
+         $currentUser = TRUE;
+         if($user['password'] != $passwordPassed)
+         {
+            $passwordError = TRUE;
+         }
       }
    }
+   if ($currentUser == FALSE)
+   {
+      echo "$usernamePassed is not a recognized username.";
+    //  header("location: login.php");
+   }
+   if ($passwordError == TRUE && $currentUser == TRUE)
+   {
+      echo "This password does not match the username's password.";
+    //  header("location: login.php");
+   }
+   $_SESSION["user"] = $usernamePassed;
 }
-if ($currentUser == FALSE)
-{
-   echo "$usernamePassed is not a recognized username.";
- //  header("location: login.php");
+else{
+   $currentUser = TRUE;
 }
-if ($passwordError == TRUE && $currentUser == TRUE)
-{
-   echo "This password does not match the username's password.";
- //  header("location: login.php");
-}
+
 
 if ($currentUser)
 {
-   session_start();
-   $_SESSION["user"] = $usernamePassed;
+
+   
    $stmt = $db->prepare("SELECT p.title FROM project p
    JOIN program_user pu ON p.program_user_id = pu.id
    WHERE pu.username = :usernamePassed;");
