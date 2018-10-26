77f04644fa407aa42cfb627aae73e8a9e728626a
shawnAshton <ash14009@byui.edu>
2018-10-25 18:32:20 -0600

    a

--- web/db_project/createProject.php
+++ web/db_project/createProject.php
@@ -38,7 +38,7 @@ $db = get_db();
       <h2>Project Title</h2>
       <input type="text" name="projectTitle"  placeholder="enter a title">
       <h2>Enter The number of meetings you want to populate</h2>
-      <input type="number" name="totalMeetings"  placeholder="1-100" min="1" max="100">
+      <input type="text" name="totalMeetings"  placeholder="1-100" maxlength="3">
 
       <h2>Add Names and Jobs</h2>
       <br/>
