d1097a29b60f1c728840a5185147d205eb396f89
shawnAshton <ash14009@byui.edu>
2018-10-30 19:09:38 -0600

    a

--- web/db_project/displayProject.php
+++ web/db_project/displayProject.php
@@ -14,7 +14,7 @@ $stmt = $db->prepare("SELECT w.name, j.job_title, jw.instance_of_meeting, p.titl
    JOIN program_user pu ON p.program_user_id = pu.id
    WHERE p.id = :project_id
    ORDER BY jw.instance_of_meeting, w.name;");
-$stmt->bindValue(":project_id", $project_id, PDO::PARAM_STR); //adds single quotes
+$stmt->bindValue(":project_id", $project_id, PDO::PARAM_INT); //adds single quotes
 $stmt->execute();
 $projects = $stmt->fetchAll(PDO::FETCH_ASSOC);
 // go through each movie in the result and display it
