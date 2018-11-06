6509687194a11987edcfad885bc145620ac82f61
shawnAshton <ash14009@byui.edu>
2018-11-06 11:38:31 -0700

    a

--- web/db_project/login.php
+++ web/db_project/login.php
@@ -9,6 +9,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+   <meta charset="utf-8">
+   <link rel="stylesheet" type="text/css" href="assignMe.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
    <title>Login</title>
 </head>
 <body>
