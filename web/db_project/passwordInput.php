301610a0b0239b10dc00153b37f013a791343d92
shawnAshton <ash14009@byui.edu>
2018-11-06 12:14:26 -0700

    a

--- web/db_project/passwordInput.php
+++ web/db_project/passwordInput.php
@@ -10,13 +10,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+   <meta charset="utf-8">
+   <link rel="stylesheet" type="text/css" href="assignMe.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
    <title>Create an Account</title>
 </head>
 <body>
    <form action="createAccount.php" method="POST">
-      New Username: <input type="text" name="newUsername"><br>
-      New Password: <input type="text" name="newPassword"><br>
-   <input type="submit" name="Create Account">
+      New Username: <input type="text" name="newUsername"><br><br>
+      New Password: <input type="text" name="newPassword"><br><br>
+   <input type="submit" value="Create Account">
    </form>
 </body>
 </html>
