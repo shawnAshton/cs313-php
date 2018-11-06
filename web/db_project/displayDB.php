a4856235d890195e16a5f9b75b67fbee749485a9
shawnAshton <ash14009@byui.edu>
2018-11-06 12:35:01 -0700

    a

--- web/db_project/displayDB.php
+++ web/db_project/displayDB.php
@@ -51,7 +51,7 @@
          // echo "$project_id" . '">';
          // echo $project_title . "</a></li>";
       }
-      echo "<br><br><hr>";
+      echo "<br><br>";
       echo "<a style='background-color: #4CAF50; color: white;' href='createProject.php'>New Job Rotation Project</a>";
    ?>
 <br><br>
