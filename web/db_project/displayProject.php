16053223a456a5f93f47fdf67bc619d6554072b0
shawnAshton <ash14009@byui.edu>
2018-11-06 12:37:03 -0700

    a

--- web/db_project/displayProject.php
+++ web/db_project/displayProject.php
@@ -30,6 +30,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+   <meta charset="utf-8">
+   <link rel="stylesheet" type="text/css" href="assignMe.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
    <title>My Project</title>
 </head>
 <body>
